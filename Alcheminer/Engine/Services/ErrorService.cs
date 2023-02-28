239e3e27cf0932a0133c62130ff1a630eb7692b2
Luramoth <85266594+Luramoth@users.noreply.github.com>
2023-02-28 01:16:09 -0800

    create ErrorService

---
+++ Alcheminer/Engine/Services/ErrorService.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace Alcheminer.Engine.Services
+{
+	internal static class ErrorService
+	{
+		public static void Error (int id)
+		{
+			switch (id)
+			{
+				case 0:
+					Debug.Fail("Chaos Engine Error: ID:0 nessesary item unspecified", "there was an item needed for your request but it was not given");
+					break;
+			}
+		}
+	}
+}
