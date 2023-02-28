0380af98bec1fda77fb802b79db53030de1ee6b3
Luramoth <85266594+Luramoth@users.noreply.github.com>
2023-02-28 01:16:24 -0800

    create Scene Service

---
+++ Alcheminer/Engine/Services/SceneService.cs
@@ -0,0 +1,18 @@
+﻿using Alcheminer.Engine.Things;
+using System.Collections.Generic;
+using static Alcheminer.Engine.Services.IDService;
+using static Alcheminer.Engine.Services.StateService;
+
+namespace Alcheminer.Engine.Services
+{
+	internal class SceneService
+	{
+
+		public class Scene
+		{
+			public Stack<Thing> Things { get; set; }
+			
+			public SceneID ID;
+		}
+	}
+}
