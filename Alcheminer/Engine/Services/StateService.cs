47f43bf03e5e8a38898bd7eecd755707b7f2daff
Luramoth <85266594+Luramoth@users.noreply.github.com>
2023-02-27 21:09:44 -0800

    make state class

--- Alcheminer/Engine/Services/StateService.cs
+++ Alcheminer/Engine/Services/StateService.cs
@@ -1,6 +1,16 @@
-﻿namespace Alcheminer.Engine.Services
+﻿using Alcheminer.Engine.Things;
+using System.Collections.Generic;
+
+namespace Alcheminer.Engine.Services
 {
 	internal static class StateService
 	{
+
+		public class State
+		{
+			public string Name { get; set; }
+
+			public Stack<Thing> Things { get; set; }
+		}
 	}
 }
