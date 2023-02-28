bb6921f36927b5421b9f766fe72dbcb3bb8cbb4f
Luramoth <85266594+Luramoth@users.noreply.github.com>
2023-02-28 01:18:21 -0800

    have images use the new Asset constructor

--- Alcheminer/Engine/Assets/Image.cs
+++ Alcheminer/Engine/Assets/Image.cs
@@ -1,6 +1,11 @@
-﻿namespace Alcheminer.Engine.Assets
+﻿using Alcheminer.Engine.Services;
+
+namespace Alcheminer.Engine.Assets
 {
-    internal class Image : Asset
-    {
-    }
+	internal class Image : Asset
+	{
+		public Image(IDService.AssetID iD) : base(iD)
+		{
+		}
+	}
 }
