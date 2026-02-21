aef86c1d072bfcbb96a9f641fcb39f4dd3c8d8df
Luramoth <fasreaperearnest@gmail.com>
2026-02-21 02:33:07 -0500

    create small transform component for testing

---
+++ AlchemyEngine/Core/Components/Transform.cs
@@ -0,0 +1,13 @@
+﻿namespace AlchemyEngine.Core.Components;
+
+/// <summary>
+/// A component that makes an Entity exist within the game world itself. it specifies position, rotation, and scale of
+/// any entity that uses it.
+/// </summary>
+public struct Transform
+{
+    /// <summary>
+    /// The global position of an object. <see cref="System.Numerics.Vector3"/>
+    /// </summary>
+    public System.Numerics.Vector3 Position;
+}
