74cf70e95072b3a54f49df3b3809ff5ce37deeda
Luramoth <fasreaperearnest@gmail.com>
2026-02-23 04:29:02 -0500

    create some physics components for testing

---
+++ AlchemyEngine/Core/Components/KinematicBody.cs
@@ -0,0 +1,14 @@
+﻿using AlchemyEngine.Core.Ecs;
+
+namespace AlchemyEngine.Core.Components;
+
+/// <summary>
+/// Base component for all <see cref="Entity"/>'s meant to be moved by physics
+/// </summary>
+public struct KinematicBody
+{
+    /// <summary>
+    /// The velocity in which an <see cref="Entity"/> is meant to move
+    /// </summary>
+    public System.Numerics.Vector3 Velocity;
+}
---
+++ AlchemyEngine/Core/Components/Rigidbody.cs
@@ -0,0 +1,14 @@
+﻿using AlchemyEngine.Core.Ecs;
+
+namespace AlchemyEngine.Core.Components;
+
+/// <summary>
+/// The core Component for all physics, does not on its own have any systems that use it, but all physics systems require it.
+/// </summary>
+public struct Rigidbody
+{
+    /// <summary>
+    /// Weight of the <see cref="Entity"/>
+    /// </summary>
+    public float Mass;
+}
