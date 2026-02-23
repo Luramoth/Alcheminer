7c09e413660b3b4fcf761ea34fd30298fc3f2278
Luramoth <fasreaperearnest@gmail.com>
2026-02-23 02:52:09 -0500

    Create type ID number system

---
+++ AlchemyEngine.Tests/Core/Ecs/ComponentTypeTest.cs
@@ -0,0 +1,16 @@
+﻿using AlchemyEngine.Core.Components;
+using AlchemyEngine.Core.Ecs;
+using JetBrains.Annotations;
+
+namespace AlchemyEngine.Tests.Core.Ecs;
+
+[TestSubject(typeof(ComponentType<>))]
+public class ComponentTypeTest
+{
+
+    [Fact]
+    public void ID_ReturnsSameIdForSameType()
+    {
+        Assert.Equal(ComponentType<Transform>.Id, ComponentType<Transform>.Id);
+    }
+}
