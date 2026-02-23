9c11e767c01aaa03319200638c4f865a58a30105
Luramoth <fasreaperearnest@gmail.com>
2026-02-22 21:20:53 -0500

    update fetcher to allow for gettign resources from passing assemblies as param

--- AlchemyEngine/IO/Fetcher.cs
+++ AlchemyEngine/IO/Fetcher.cs
@@ -1,7 +1,11 @@
 ﻿using System.Diagnostics;
+using System.Reflection;
 
 namespace AlchemyEngine.IO;
 
+/// <summary>
+/// Class in which to retrieve Resources from
+/// </summary>
 public class Fetcher
 {
     /// <summary>
@@ -13,6 +17,26 @@ public class Fetcher
     public static string GetFileContents(string path)
     {
         using var stream = (Runtime.WasCalledFromEngineAssembly()) ? Runtime.AlchemyAssembly.GetManifestResourceStream(path) : Runtime.GameAssembly.GetManifestResourceStream(path);
+
+        if (stream == null)
+        {
+            return "null";
+        }
+
+        using var reader = new StreamReader(stream);
+        return reader.ReadToEnd();
+    }
+
+    /// <summary>
+    /// Will grab the contents of a file stored inside the executor's assembly Through EmbeddedResource.
+    /// To use it's recommended you put Include="Assets\**\*" in a EmbeddedResource tag inside the ItemGroup tag inside your game's .csproj file.
+    /// </summary>
+    /// <param name="path">A string specifying the path to your resource. ex: "Assets/Shader/shader.slang" would be "{GameNamespace}.Assets.Shader.shader.slang"</param>
+    /// <param name="assembly">The assembly to get the EmbeddedResource from</param>
+    /// <returns>A string with the file's contents</returns>
+    public static string GetFileContents(string path, Assembly assembly)
+    {
+        using var stream = assembly.GetManifestResourceStream(path);
 
         if (stream == null)
         {
