ed9692559b0f6fbe643db8af9728a50ebe0ddbbe
Luramoth <fasreaperearnest@gmail.com>
2026-02-22 21:20:31 -0500

    Start unit testing

---
+++ AlchemyEngine.Tests/RuntimeTest.cs
@@ -0,0 +1,21 @@
+﻿using AlchemyEngine;
+using JetBrains.Annotations;
+
+namespace AlchemyEngine.Tests;
+
+[TestSubject(typeof(Runtime))]
+public class RuntimeTest
+{
+
+    [Fact]
+    public void WasCalledFromEnmgineAssembly_ReturnsFalseWhenCalledFromTest()
+    {
+        Assert.False(Runtime.WasCalledFromEngineAssembly());
+    }
+
+    [Fact]
+    public void Init_RunsNominally()
+    {
+        Runtime.Init();
+    }
+}
